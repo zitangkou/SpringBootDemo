8c9b9995f6bcb47276c700ccc5ebb7847c5354c3
邓宁宁 <deng.ningning@iwhalecloud.com>
2022-09-03 13:15:07 +0800

    First commit.

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM serendibsystems/java17
+
+VOLUME /tmp 
+
+ADD SpringBootDemo-0.0.1-SNAPSHOT.jar app.jar
+
+RUN bash -c 'touch /app.jar'
+
+EXPOSE 8080
+
+ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/app.jar"]
--- src/main/java/com/example/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/com/example/springbootdemo/SpringBootDemoApplication.java
@@ -2,7 +2,6 @@ package com.example.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class SpringBootDemoApplication {
